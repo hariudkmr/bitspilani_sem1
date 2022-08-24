479fd1fe832bfa65e7da290a3ff370d47a91a859
Hari Udayakumar <hariudkmr@gmail.com>
2022-08-24 23:06:31 +0530

    1. Fixed the error in Task1

--- MikroC_WS/Task1/Task1.c
+++ MikroC_WS/Task1/Task1.c
@@ -4,13 +4,14 @@ TRISD = 0x00;
 
   while(1)
   {
+      
       while(!(PORTB & 0x01))
       {
          PORTD ^= 0x03;
          Delay_ms(1000);
       }
 
-      while(!(PORTB & 0x01))
+      while(!(PORTB & 0x02))
       {
          PORTD ^= 0xC0;
          Delay_ms(1000);
