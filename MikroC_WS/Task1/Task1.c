7d46620a08a3f3aee50a5d19502b5ccbb546f4d8
Hari Udayakumar <hariudkmr@gmail.com>
2022-08-24 22:48:59 +0530

    1.Added ES_Assignment_Task1

---
+++ MikroC_WS/Task1/Task1.c
@@ -0,0 +1,26 @@
+void main() {
+TRISB = 0x07;
+TRISD = 0x00;
+
+  while(1)
+  {
+      while(!(PORTB & 0x01))
+      {
+         PORTD ^= 0x03;
+         Delay_ms(1000);
+      }
+
+      while(!(PORTB & 0x01))
+      {
+         PORTD ^= 0xC0;
+         Delay_ms(1000);
+      }
+
+      while(!(PORTB & 0x04))
+      {
+         PORTD ^= 0xE7;
+         Delay_ms(1000);
+      }
+      PORTD = 0x00;
+    }
+}
