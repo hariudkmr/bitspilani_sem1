3892ca0d125fb8795df48a48339b09bc7a344797
Hari Udayakumar <hariudkmr@gmail.com>
2022-08-27 19:49:23 +0530

    Proteus - task1 project

--- MikroC_WS/Task1/Task1.c
+++ MikroC_WS/Task1/Task1.c
@@ -1,23 +1,24 @@
 void main() {
-TRISB = 0x07;
-TRISD = 0x00;
+TRISB = 0x07;   //RB0, RB1, RB2 as Input Pins
+TRISD = 0x00;   //All the Pins in Port configured as Output
+PORTD = 0x00;
 
   while(1)
   {
-      
-      while(!(PORTB & 0x01))
+      //W
+      while((PORTB & 0x01))
       {
          PORTD ^= 0x03;
          Delay_ms(1000);
       }
 
-      while(!(PORTB & 0x02))
+      while((PORTB & 0x02))
       {
          PORTD ^= 0xC0;
          Delay_ms(1000);
       }
 
-      while(!(PORTB & 0x04))
+      while((PORTB & 0x04))
       {
          PORTD ^= 0xE7;
          Delay_ms(1000);
